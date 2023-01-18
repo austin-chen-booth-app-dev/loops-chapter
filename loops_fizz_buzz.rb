84ba9d9d17d319882f096a383b4fe85b04521439
austin-chen-booth <achen52@chicagobooth.edu>
2023-01-18 17:46:28 +0000

    Homework

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -4,3 +4,23 @@
 # for multiples of three print "Fizz" instead of the number
 #   for multiples of five print "Buzz" instead of the number
 #   for numbers which are multiples of both three and five print "FizzBuzz"
+
+for i in (1..100)
+  print_num = true
+
+  if i % 3 == 0
+    print("Fizz")
+    print_num = false
+  end
+
+  if i % 5 == 0
+    print("Buzz")
+    print_num = false
+  end
+
+  if print_num
+    print(i)
+  end
+
+  print("\n")
+end
--- loops_multiples.rb
+++ loops_multiples.rb
@@ -1,9 +1,9 @@
 # Write a program that:
-# 
+#
 # Asks the user to input a number. The program should print multiplication table of entered number from 1 to 10.
-# 
+#
 # Example (`2` is the input):
-# 
+#
 # "Enter a number:"
 # 2
 # 2
@@ -18,4 +18,14 @@
 # 20
 
 p "Enter a number:"
+num = gets.chomp.to_i
 
+(1..11).step(1).each do |i|
+  p(i * num)
+end
+
+# or a for loop?
+
+for i in 1..11
+  p(i * num)
+end
